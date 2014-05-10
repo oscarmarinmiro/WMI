6c3a62bb35b988c69b49814f38aab066cc761f9a
alex <alejandrogonzalezdiez@gmail.com>
2014-05-10 17:52:09 +0200

    getES.py preparado a falta de arqueologia de bugs :D

--- bin/getES.py
+++ bin/getES.py
@@ -1,14 +1,22 @@
 __author__ = 'Oscar'
 
 import random
+from elasticsearch import Elasticsearch
+import time
+
+def getTweetsFromWords(words,lang="en",maxDocs=100,index="twitter_river"):
+    es = Elasticsearch([{'host':'tesla','port':9200}])
+    res = es.search(index="twitter_river",q="text:"+words+" AND lang:"+lang,fields="text")
+    results = list()
+    for hit in res['hits']['hits']:
+        results.append(hit['fields']['text'])
+
+    return results
 
 
-def getTweetsFromWords(words,lang):
+def getCountFromWords(words, lang="en"):
 
+    es = Elasticsearch([{'host':'tesla','port':9200}])
+    res = es.search(index="twitter_river",q="text:"+" AND ".join(words)+" AND lang:"+lang,fields="text")
 
-    return []
-
-
-def getCountFromWords(words, lang):
-
-    return random.randint(1000000, 10000000)
+    return res['hits']['total']
