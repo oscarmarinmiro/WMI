c70bc2710e897ee5114bf76ebe1b444aa40c5a6b
alex <alejandrogonzalezdiez@gmail.com>
2014-05-10 18:28:42 +0200

    Ahora con requests... :D

--- bin/getES.py
+++ bin/getES.py
@@ -3,25 +3,46 @@ __author__ = 'Oscar'
 import random
 from elasticsearch import Elasticsearch
 import time
+import requests
+import json
 
 def getTweetsFromWords(words,lang="en",maxDocs=100,index="twitter_river"):
     
     results = list()
 
-    es = Elasticsearch([{'host':'tesla','port':9200}])
-  
-    res = es.search(index=index,q="text:"+words+" AND lang:"+lang,fields="text")
+    url = "http://assets.outliers.es/es/twitter_river/_search?q=text%3Ahola+AND+lang%3A"+lang+"&fields=text"
+    res = requests.get(url)
 
-    for hit in res['hits']['hits']:
-        results.append(hit['fields'])
+    data = json.loads(res.text)['hits']['hits']
+
+    print data
+
+    for entry in data:
+        print entry
+        results.append(entry['fields'])
+
+    #es = Elasticsearch([{'host':'assets.outliers.es/es','port':80}])
+  
+    #res = es.search(index=index,q="text:"+words+" AND lang:"+lang,fields="text")
+
+    #print res['hits']['hits']
+
+    #for hit in res['hits']['hits']:
+    #    results.append(hit['fields'])
 
     
     return results
 
 
-def getCountFromWords(words, lang="en"):
+def getCountFromWords(words, lang="en", index="twitter_river"):
 
-    es = Elasticsearch([{'host':'tesla','port':9200}])
-    res = es.search(index="twitter_river",q="text:"+" AND ".join(words)+" AND lang:"+lang,fields="text")
+    query = " AND ".join(words)
+    url = "http://assets.outliers.es/es/twitter_river/_search?q="+query+"+AND+lang%3A"+lang+"&fields=text"
+    res = requests.get(url)
 
-    return res['hits']['total']
+    count = json.loads(res.text)['hits']['total']
+    #es = Elasticsearch([{'host':'tesla','port':9200}])
+
+    #res = es.search(index=index,q="text:"+" AND ".join(words)+" AND lang:"+lang,fields="")
+
+    return count
